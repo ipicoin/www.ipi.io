c37b24eda49dc417c3a315adbb5df0675781204d
Sett Sarverott <sett@sarverott.com>
2026-06-10 11:54:10 +0200

    massive upgrade of project

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'IPI.IO',
+			social: [{ icon: 'github', label: 'IPI DAO on GitHub', href: 'https://github.com/ipicoin' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					items: [{ autogenerate: { directory: 'reference' } }],
+				},
+			],
+		}),
+	],
+});
